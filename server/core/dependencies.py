e2fa1f57a551237d07c5cce5226496f5e81ada88
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-04-05 15:51:57 +0100

    Backend. feat(user): add 'profile_create' and 'get_user_profile' services. Add 'get_current_user' dependency

---
+++ server/core/dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, \
+    HTTPAuthorizationCredentials
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models.user import UserModel
+from database.session_postgresql import get_db
+from utils.tokens import decode_access_token
+
+security = HTTPBearer()
+
+async def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        db: AsyncSession = Depends(get_db)
+):
+    token = credentials.credentials
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials"
+        )
+
+    user_id = payload.get("sub")
+    user_stmt = (
+        select(UserModel).where(UserModel.id == int(user_id))
+    )
+    result = await db.execute(user_stmt)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+
+    return user
