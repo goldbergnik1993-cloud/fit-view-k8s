e99828baef793346c7f1a6c897348a77e1cd0809
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-04-05 15:53:41 +0100

    Backend. feat(user): expose profile management endpoints (GET and POST) via /user/profile

--- server/api/user.py
+++ server/api/user.py
@@ -2,15 +2,23 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.dependencies import get_current_user
+from database.models.user import UserModel
 from database.session_postgresql import get_db
 from schemas.user import (
     UserCreateSchema,
     UserRetrieveSchema,
     LoginSchema,
     TokenPairResponse,
-    RefreshTokenRequest
+    RefreshTokenRequest, ProfileBaseSchema, ProfileViewSchema
 )
-from services.user import user_create, user_login, refresh_token_pair
+from services.user import (
+    user_create,
+    user_login,
+    refresh_token_pair,
+    profile_create,
+    get_user_profile
+)
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -39,3 +47,20 @@ async def refresh_access_token(
 ):
     token_pair = await refresh_token_pair(payload=payload, db=db)
     return token_pair
+
+
+@router.post("/profile", response_model=ProfileViewSchema)
+async def create_user_profile(
+        payload: ProfileBaseSchema,
+        db: AsyncSession = Depends(get_db),
+        current_user: UserModel = Depends(get_current_user)
+):
+    return await profile_create(payload=payload, db=db, user=current_user)
+
+
+@router.get("/profile", response_model=ProfileViewSchema)
+async def my_profile(
+        db: AsyncSession = Depends(get_db),
+        current_user: UserModel = Depends(get_current_user)
+):
+    return await get_user_profile(db=db, user=current_user)
