dfffa6868b9b9188afc71d665486eedb9ace76a4
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-04-06 16:17:02 +0100

    Backend. feat(catalog): add RoleChecker dependency for user access delimitation, add pagination_helper for pagination handling

--- server/core/dependencies.py
+++ server/core/dependencies.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, HTTPBearer, \
     HTTPAuthorizationCredentials
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models.user import UserModel
+from database.models.user import UserModel, UserRoleEnum
 from database.session_postgresql import get_db
 from utils.tokens import decode_access_token
 
@@ -36,3 +38,16 @@ async def get_current_user(
         )
 
     return user
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List[UserRoleEnum]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: UserModel = Depends(get_current_user)):
+        if user.role not in self.allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have enough permissions to access this resource."
+            )
+        return user
---
+++ server/utils/service_helpers.py
@@ -0,0 +1,48 @@
+from urllib.parse import urlencode
+
+from fastapi import Request
+from sqlalchemy import Select, select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def generate_page_link(
+        request: Request,
+        page_number: int,
+        per_page: int
+) -> str:
+    params = dict(request.query_params)
+    params["page"] = page_number
+    params["per_page"] = per_page
+
+    return f"{request.url.path}?{urlencode(params)}"
+
+
+async def pagination_helper(
+        request: Request,
+        page: int,
+        per_page: int,
+        db: AsyncSession,
+        stmt: Select,
+        scalars: bool = True
+) -> dict:
+    offset = (page - 1) * per_page
+    count_stmt = select(func.count()).select_from(stmt.subquery())
+    total_items = (await db.execute(count_stmt)).scalar() or 0
+    total_pages = (total_items + per_page - 1) // per_page
+
+    prev_page = await generate_page_link(
+        request, page - 1, per_page
+    ) if page > 1 else None
+    next_page = await generate_page_link(
+        request, page + 1, per_page
+    ) if page < total_pages else None
+
+    items = await db.execute(stmt.offset(offset).limit(per_page))
+
+    return {
+        "items": items.scalars().all() if scalars else items.all(),
+        "total_pages": total_pages,
+        "total_items": total_items,
+        "prev_page": prev_page,
+        "next_page": next_page,
+    }
