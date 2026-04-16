a5505d6aa1f46829a2a9e4ba51dea593395f77a6
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-04-16 22:08:16 +0100

    Backend. feat(tasks): implement async email dispatch and scheduled cron jobs

---
+++ server/tasks/cleanup_tasks.py
@@ -0,0 +1,41 @@
+import asyncio
+from datetime import datetime, timezone, timedelta
+from sqlalchemy import delete, update
+from core.celery_app import celery_app
+from database.models import RefreshTokenModel, CartModel
+from database.models.cart import CartStatusEnum
+from database.session_postgresql import SessionLocal
+
+
+async def _cleanup_logic():
+    async with SessionLocal() as db:
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
+        await db.execute(
+            delete(RefreshTokenModel).where(
+                RefreshTokenModel.expires_at < now)
+        )
+        await db.commit()
+
+
+async def _change_cart_status_logic():
+    async with SessionLocal() as db:
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
+        await db.execute(
+            update(CartModel)
+            .where(
+                CartModel.created_at < now - timedelta(hours=24),
+                CartModel.status == CartStatusEnum.ACTIVE
+            )
+            .values(status=CartStatusEnum.ABANDONED)
+        )
+        await db.commit()
+
+
+@celery_app.task(name="tasks.cleanup_tasks.cleanup_expired_tokens")
+def cleanup_expired_tokens():
+    asyncio.run(_cleanup_logic())
+
+
+@celery_app.task(name="tasks.cleanup_tasks.mark-carts-abandoned")
+def mark_carts_abandoned():
+    asyncio.run(_change_cart_status_logic())
