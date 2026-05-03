48b09fcf3047ac3afac807794416dd48134c60d7
Anastasia <nastyapugach24@gmail.com>
2026-05-03 21:47:38 +0200

    fixed hero section

--- client/src/pages/Home/components/HeroSection/HeroSection.tsx
+++ client/src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -13,20 +13,10 @@ export const HeroSection = () => {
         <div className={styles['hero__content']}>
           <div className={styles['hero__media']} aria-hidden="true">
             <div className={styles['hero__silhouette']}>
-              <img
-                src={SilhouetteManImg}
-                alt=""
-                aria-hidden="true"
-                className={styles['hero__silhouette-img']}
-              />
+              <img src={SilhouetteManImg} alt="" aria-hidden="true" className={styles['hero__silhouette-img']} />
             </div>
             <div className={styles['hero__silhouette']}>
-              <img
-                src={SilhouetteWomanImg}
-                alt=""
-                aria-hidden="true"
-                className={styles['hero__silhouette-img']}
-              />
+              <img src={SilhouetteWomanImg} alt="" aria-hidden="true" className={styles['hero__silhouette-img']} />
             </div>
           </div>
 
@@ -35,8 +25,7 @@ export const HeroSection = () => {
               Find your new favorite style without the hassle
             </h1>
             <p className={styles['hero__description']}>
-              Browse, measure, and virtually try on in seconds. Check out these
-              brief{' '}
+              Browse, measure, and virtually try on in seconds. Check out these brief{' '}
               <button
                 className={styles['hero__instructions-link']}
                 onClick={() => setIsModalOpen(true)}
@@ -46,15 +35,16 @@ export const HeroSection = () => {
               </button>{' '}
               before you get started
             </p>
-            <a
-              href="/catalog"
-              className={styles['hero__cta']}
-              aria-label="Start virtual try-on, go to catalog"
-            >
-              Start Virtual Try-On
-            </a>
           </div>
         </div>
+
+        <a
+          href="/catalog"
+          className={styles['hero__cta']}
+          aria-label="Start virtual try-on, go to catalog"
+        >
+          Start Virtual Try-On
+        </a>
       </section>
 
       <OnboardingModal
