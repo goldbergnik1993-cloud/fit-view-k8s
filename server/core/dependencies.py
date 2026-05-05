e8a3ec410c88484f309d2a4dee96a7f732f4cf6b
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-05-05 18:20:10 +0100

    Backend. refactor(api): moved user fetching logic to a dedicated function in dependencies
    fix(static): updated .gitignore, added 'static' directory to project structure

--- server/core/dependencies.py
+++ server/core/dependencies.py
@@ -7,6 +7,7 @@ from redis import Redis
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+# from core import redis_client
 from core.logging_config import logger
 from core.redis_client import get_redis
 from database.models.user import UserModel, UserRoleEnum
@@ -17,21 +18,9 @@ security = HTTPBearer()
 optional_security = HTTPBearer(auto_error=False)
 
 
-async def get_current_user(
-    credentials: HTTPAuthorizationCredentials = Depends(security),
-    db: AsyncSession = Depends(get_db),
-    redis_client: Redis = Depends(get_redis),
-):
-    token = credentials.credentials
-    payload = decode_access_token(token)
-    if not payload:
-        logger.warning("auth_failed", reason="invalid_token")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
-
-    user_id = payload.get("sub")
+async def get_user_by_id(
+        user_id: int, db: AsyncSession, redis_client: Redis
+) -> UserModel:
     cache_key = f"auth_user:{user_id}"
 
     cached_user_str = await redis_client.get(cache_key)
@@ -60,9 +49,30 @@ async def get_current_user(
     return user
 
 
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: AsyncSession = Depends(get_db),
+    redis_client: Redis = Depends(get_redis),
+):
+    token = credentials.credentials
+    payload = decode_access_token(token)
+    if not payload:
+        logger.warning("auth_failed", reason="invalid_token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
+
+    user_id = payload.get("sub")
+    return await get_user_by_id(
+        user_id=user_id, db=db, redis_client=redis_client
+    )
+
+
 async def get_optional_current_user(
     credentials: Optional[HTTPAuthorizationCredentials] = Depends(optional_security),
     db: AsyncSession = Depends(get_db),
+    redis_client: Redis = Depends(get_redis),
 ) -> Optional[UserModel]:
     if not credentials:
         return None
@@ -75,13 +85,9 @@ async def get_optional_current_user(
         return None
 
     user_id = payload.get("sub")
-    if not user_id:
-        logger.warning("auth_failed", reason="user_not_found", user_id=user_id)
-        return None
-
-    user_stmt = select(UserModel).where(UserModel.id == int(user_id))
-    result = await db.execute(user_stmt)
-    return result.scalar_one_or_none()
+    return await get_user_by_id(
+        user_id=user_id, db=db, redis_client=redis_client
+    )
 
 
 async def get_user_by_email(email: str, db: AsyncSession) -> Optional[UserModel]:
