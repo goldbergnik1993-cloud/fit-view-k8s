a629a4541ba9d0f0b3c44bd4fa254ad1c89c6ffb
Anastasia <nastyapugach24@gmail.com>
2026-05-08 17:07:50 +0200

    fix

--- client/src/pages/Home/components/HeroSection/HeroSection.tsx
+++ client/src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -30,10 +30,11 @@ export const HeroSection = () => {
             </div>
           </div>
 
+          <h1 className={styles['hero__title']}>
+            Find your new favorite style without the hassle
+          </h1>
+
           <div className={styles['hero__text']}>
-            <h1 className={styles['hero__title']}>
-              Find your new favorite style without the hassle
-            </h1>
             <p className={styles['hero__description--inline']}>
               Browse, measure, and virtually try on in seconds. Check out these
               brief{' '}
